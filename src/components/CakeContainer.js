5d8ddaf267af7986c0f822182eeccd0c0828fc8a
Alex-Choi <choijeaho86@gmail.com>
2021-09-19 05:22:37 +0900

    error invalid hook call

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import { buyCake } from '../redux';
 
-const CakeContainer = () => {
+const CakeContainer = (props) => {
+  console.log(props);
   return (
     <div>
-      <h2>Number of cakes</h2>
-      <button>Buy Cake</button>
+      <h2>Number of cakes : {props.numOfCakes}</h2>
+      <button onClick={props.buyCake}>Buy Cake</button>
     </div>
   )
 }
 
-export default CakeContainer
+const mapStateToProps = state => {
+  return {
+    numOfCakes: state.numberOfCake
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    buyCake : () => dispatch(buyCake())
+  }
+}
+
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+  )(CakeContainer)
